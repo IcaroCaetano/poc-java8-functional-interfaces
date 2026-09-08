3b6643001500ec6aa891c2129e10adebe8617a93
ICAROCAETANO <icaro.caetano@gmail.com>
2026-09-07 22:54:55 -0300

    first commit

---
+++ src/main/java/com/javaproject/poc_java8_functional_interfaces/PocJava8FunctionalInterfacesApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.poc_java8_functional_interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJava8FunctionalInterfacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJava8FunctionalInterfacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaproject/poc_java8_functional_interfaces/PocJava8FunctionalInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.poc_java8_functional_interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJava8FunctionalInterfacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
